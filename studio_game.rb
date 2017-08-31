a0f526375142f01a0a7336fbe15a51c0067e1665
QuantumWaver <sholl76@gmail.com>
2017-08-31 13:24:59 -0400

    Add chapter 3 exercises

--- studio_game.rb
+++ studio_game.rb
@@ -1,1 +1,22 @@
-puts "Welcome to THE GAME!"
+greeting = "Welcome to THE GAME!"
+
+3.times do
+  puts greeting.upcase
+end
+
+puts "Time Started: #{Time.now}"
+
+
+player_one_name = 'larry'
+player_one_health = 60
+
+player_two_name = 'curly'
+player_two_health = 60
+
+player_three_name = 'moe'
+player_three_health = 60
+
+
+puts "#{player_one_name}'s health is #{player_one_health}"
+
+puts "Players:\n\t#{player_one_name}\n\t#{player_two_name}\n\t#{player_three_name}"
